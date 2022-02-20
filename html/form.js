cac1aa3ccf099991df9fadeeede00c2b794f6604
jfurdzik <jfurdzik@mit.edu>
2022-02-20 16:48:24 -0500

    !!! form info

--- html/form.js
+++ html/form.js
@@ -1,24 +1,71 @@
 
 function getDate() { 
     let date = document.querySelector('#taskDate').value;
-    //console.log(date);
+    let name = document.querySelector('#taskName').value;
+    console.log(date);
+    const entriesString = sessionStorage.getItem("entries");
+    let entries = {rows: []};
+    if (entriesString != null)
+    {
+        entries = JSON.parse(entriesString);
+    }
     //addRow();
-
+    
+    const entry = {
+        date: date,
+        name: name,
+    }
+    entries.rows.push(entry);
+    sessionStorage.setItem("entries", JSON.stringify(entries));
+    
     //then query selector again and access task.html, make new cell row below (HOW THO)
     //prbly gonna have to use innerText, querySelector, create element, and/or the `words template ${date}` thing
 }
 
 function addRow()
 {
-    var str="";
+    const table = document.querySelector(".task-table-container tbody");
+    console.log(table);
+
+    const entriesString = sessionStorage.getItem("entries");
+    if (entriesString != null)
+    {
+        const entries = JSON.parse(entriesString);
+        console.log(entries);
+        for (let i=0; i<entries.rows.length; i++)
+        {
+            const tr = document.createElement("tr");
+            
+            const dateth = document.createElement("th");
+            dateth.innerHTML = entries.rows[i].date;
+            tr.appendChild(dateth);
+
+            const nameth = document.createElement("th");
+            nameth.innerHTML = entries.rows[i].name;
+            tr.appendChild(nameth);
+
+            table.appendChild(tr);
+        }
+        
+        //repeat 30-32 for each new field
+    }
+    
+    /*var str="";
     for(var i=0;i<20;i++)
     {
         str+="<tr>";
         str+="<td>"+i+"</td>";
         str+="</tr>";
     }
-    $('#add').append(str); //insert row
+    $('#add').append(str); //insert row*/
 }
 //in html FORM submit button -> onClick="getDate()" 
 //also add <script src="form.js"></script> to end of ^ file
 
+window.onload = function() {
+    if (window.location.href.indexOf("task.html") > -1)
+    {
+        addRow();
+    }
+    
+}
