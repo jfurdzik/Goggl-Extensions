d2e31972feb47b63d3048f4878ee6bc69b94be39
jfurdzik <jfurdzik@mit.edu>
2022-02-20 15:26:04 -0500

    Update form.js

--- html/form.js
+++ html/form.js
@@ -1,1 +1,9 @@
-let date = document.querySelector('#taskDate').value;
+function getDate() { 
+    let date = document.querySelector('#taskDate').value;
+    console.log(date);
+    //then query selector again and access task.html, make new cell row below (HOW THO)
+    //prbly gonna have to use innerText, querySelector, create element, and/or the `words template ${date}` thing
+}
+
+//in html FORM submit button -> onClick="getDate()" 
+//also add <script src="form.js"></script> to end of ^ file
